3b223fdbdb58c99f4f20b32a7a005856186df28e
Deepesh <sainideepesh12@gmail.com>
2025-01-29 11:55:05 +0530

    add buttons

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function RandomColor() {
     return colours[Math.floor(Math.random() * colours.length)];
 }
 
+function updateButton() {
+    undobtn.disabled = undoCircles.length === 0;
+    redobtn.disabled = redoCircles.length === 0;
+    reset.disabled = undoCircles.length === 0 && redoCircles.length === 0;
+}
+
+showResult.addEventListener("click", (e) => {
+    if (e.target !== showResult) return;
+    const x = e.pageX;
+    const y = e.pageY;
+
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+
+    circle.style.width = "20px";
+    circle.style.height = "20px";
+
+    circle.style.left = `${x - 10}px`;
+    circle.style.top = `${y - 10}px`;
+
+    const color =RandomColor();
+    circle.style.backgroundColor = color;
+
+    showResult.appendChild(circle);
+    undoCircles.push(circle);
+    redoCircles.length = 0;
+
+    updateButton();
+});
